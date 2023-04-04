1da6362b81a024c48ecdb5df4fc7d45c719297db
Shashankranjan-dev <shashankranjan970832@gmail.com>
2023-04-05 01:22:54 +0530

    color change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
               <Link
                 to="/about"
                 href="#1"
-                class="block py-2 pl-3 pr-4 text-gray-900 
+                class="block py-2 pl-3 pr-4 text-white
                
                 font-bold font-assistant tracking-widest rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
